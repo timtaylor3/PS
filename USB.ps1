d0daeeeb2f6ea7623c6ebf0fbcac565ce8145025
Tim Taylor <timtaylor3@yahoo.com>
2017-08-03 15:43:59 -0500

    Update USB.ps1

--- USB.ps1
+++ USB.ps1
@@ -1,9 +1,14 @@
-﻿$DRIVE= Read-Host -Prompt "Enter Mounted Drive Letter"
-$USER_NAME = Read-Host -Prompt "Enter User Name"
-$USER_PROFILE = $DRIVE + "\Users\" + $USER_NAME
-$SYSTEM = $DRIVE + "\Windows\System32\config\SYSTEM"
-
+$DRIVE= Read-Host -Prompt "Enter Mounted Drive Letter"
+$PATH = $DRIVE + '\Users' 
+$USER_DIR = Get-ChildItem -Path $PATH -Filter NTUSER.DAT -Recurse -Depth 1 -Name -ErrorAction SilentlyContinue -Force
 Get-ForensicRegistryKey -HivePath $SYSTEM -Key ControlSet001\Enum\USBSTOR | Format-Table Name
 Get-ForensicRegistryValue -HivePath $SYSTEM -Key MountedDevices | Format-Table Name
-Get-ForensicRegistryKey -HivePath ( $USER_PROFILE + "\NTUSER.DAT" ) -Key SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2 | Sort-Object WriteTime | Format-Table Name, WriteTime
+Foreach($FILE in $USER_DIR)
+{ 
+   If ($FILE -ne "Default\NTUSER.DAT") {
+      $USER_REG = $PATH + '\' + $FILE
+      Write-Host $USER_REG
+      Get-ForensicRegistryKey -HivePath $USER_REG -Key SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\MountPoints2 | Sort-Object WriteTime | Format-Table Name, WriteTime
+   } 
+}
 Get-ForensicRegistryKey -HivePath $SYSTEM -Key ControlSet001\Enum\USB | Sort-Object WriteTime | Format-Table Name, WriteTime
